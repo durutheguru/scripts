cb6068188e6072bc80673e9d7e7307b4024649fa
Julian Duru <durutheguru@gmail.com>
2022-04-11 22:37:21 +0100

    - Modifications to spring_init sh

--- bash/spring_init.sh
+++ bash/spring_init.sh
@@ -8,5 +8,5 @@ read -p "artifact: " artifact
 read -p "group: " group
 read -p "dependencies: " dependencies
 
-spring init -a=$artifact -g=$group --build=maven -j=$javaVersion -d=$dependencies $artifact
+spring init -a=$artifact -g=$group --build=maven -j=$javaVersion -n=$artifact -d=$dependencies $artifact
 
---
+++ bash/spring_packages_rm.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+rm -r dto
+rm -r controller
+rm -r service
+rm -r util
+rm -r entity
+rm -r repository
+
