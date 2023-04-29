3b5da970cd5f07b8e6ef1e482f03d995cf974106
Julian Duru <durutheguru@gmail.com>
2023-04-29 18:00:00 +0100

    Code updates

--- docker/debezium/install_connector.sh
+++ docker/debezium/install_connector.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" localhost:8083/connectors/ -d '{
-  "name": "lambda_crud-connector-2",
+  "name": "lambda_crud-connector",
   "config": {
     "connector.class": "io.debezium.connector.mysql.MySqlConnector",
     "tasks.max": "1",
@@ -11,10 +11,9 @@ curl -i -X POST -H "Accept:application/json" -H "Content-Type:application/json" 
     "database.password": "1234567890",
     "database.allowPublicKeyRetrieval":"true",
     "database.server.id": "184054",
-    "topic.prefix": "dbserver1",
+    "topic.prefix": "dbserver2",
     "database.include.list": "lambda_crud",
     "schema.history.internal.kafka.bootstrap.servers": "kafka:9092",
     "schema.history.internal.kafka.topic": "schema-changes.lambda_crud"
   }
 }'
-
