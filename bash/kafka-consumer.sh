b993f310e700a1a243a4370e08cfea286d1aa198
Julian Duru <durutheguru@gmail.com>
2023-05-08 12:00:01 +0100

    Code updates

--- bash/kafka-consumer.sh
+++ bash/kafka-consumer.sh
@@ -2,5 +2,6 @@
 
 
 read -p "Enter Topic Name: " topic_name
+read -p "Enter Bootstrap Server(s): " bootstrap_servers
 
-kafka-console-consumer.sh -bootstrap-server localhost:9092 --topic $topic_name --from-beginning
+kafka-console-consumer.sh -bootstrap-server $bootstrap_servers --topic $topic_name --from-beginning
