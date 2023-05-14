714cc8f7d074e022f71c95e1a448107cb7db0dd4
Julian Duru <durutheguru@gmail.com>
2023-05-14 22:54:36 +0100

    Updated: bash/git_auto_commit.sh

--- bash/git_auto_commit.sh
+++ bash/git_auto_commit.sh
@@ -3,11 +3,6 @@
 
 git add .
 
-files=$(git diff --name-status | awk '$1 == "M" {print "Updated: "$2} $1 == "A" {print "Created: "$2}')
-
-export message=$(echo -e "Files:$files")
-
-git commit -m "$message"
-
+git diff --name-status | awk '$1 == "M" {print "Updated: "$2} $1 == "A" {print "Created: "$2}' | git commit -aF -
 
 
