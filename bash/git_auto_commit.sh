37c8198dc191ab8951b452c7c7b1264e22b2a948
Julian Duru <durutheguru@gmail.com>
2023-05-14 22:43:54 +0100

    Updated and created files:\n

--- bash/git_auto_commit.sh
+++ bash/git_auto_commit.sh
@@ -5,12 +5,9 @@ git add .
 
 files=$(git diff --name-status | awk '$1 == "M" {print "Updated: "$2} $1 == "A" {print "Created: "$2}')
 
-if [ -z "$files" ]; then
-    message="..."
-else
-    message="Updated and created files:\n$files"
-fi
-
+message="Updated and created files:\n$files"
 
 git commit -m "$message"
+
+
 
