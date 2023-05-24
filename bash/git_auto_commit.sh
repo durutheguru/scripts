b2be93c18f763614005bdb1f6a4620ae4eb22ffd
Julian Duru <durutheguru@gmail.com>
2023-05-25 00:00:01 +0100

    Updated: bash/git_auto_commit.sh

--- bash/git_auto_commit.sh
+++ bash/git_auto_commit.sh
@@ -1,10 +1,22 @@
 #!/bin/bash 
 
 
+files=()
 
-git diff --name-status | awk '$1 == "M" {print "Updated: "$2} $1 == "A" {print "Created: "$2}' > changes.txt
-git add -A
-git commit -F changes.txt
-rm changes.txt
+while IFS= read -r line; do
+  status=$(echo "$line" | awk '{print $1}')
+  file=$(echo "$line" | awk '{print $2}')
+  
+  if [[ $status =~ ^(M|A|D|\?\?)$ ]]; then
+    files+=("$status: $file")
+  fi
+done < <(git status --porcelain)
 
+# Commit the changes
+if [[ ${#files[@]} -gt 0 ]]; then
+  commit_message=$(printf "%s\n" "${files[@]}")
+  git commit -am "$commit_message"
+else
+  echo "No changes to commit."
+fi
 
