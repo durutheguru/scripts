8398fb836e76ee726fb993b188127dd0b93d4b50
Julian Duru <durutheguru@gmail.com>
2023-07-18 14:00:01 +0100

    Updated: bash/kafka-producer.sh

--- bash/kafka-producer.sh
+++ bash/kafka-producer.sh
@@ -1,6 +1,16 @@
 #! /bin/bash
 
-read -p "Enter Topic Name: " topic_name
-read -p "Enter Broker URL: " broker_url
 
-kafka-console-producer.sh --broker-list $broker_url --topic $topic_name
+if [ $# == 2 ];
+then
+    kafka-console-producer.sh --broker-list ${1} --topic ${2}
+else
+    read -p "Enter Topic Name: " topic_name
+    read -p "Enter Broker URL: " broker_url
+
+    kafka-console-producer.sh --broker-list $broker_url --topic $topic_name
+fi
+
+
+
+
